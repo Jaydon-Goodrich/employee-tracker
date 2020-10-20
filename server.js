1388247487fe6710636d9692179eab37a9d367af
Jaydon-Goodrich <43736820+Jaydon-Goodrich@users.noreply.github.com>
2020-10-19 18:37:22 -0600

    Update server.js
    Removed password

--- server.js
+++ server.js
@@ -59,7 +59,7 @@ const connection = mysql.createConnection({
   // Your MySQL username
   user: 'root',
   // Your MySQL password
-  password: 'Jan221997',
+  password: 'password',
   database: 'employeesDB'
 });
 
